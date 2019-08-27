5f8dc6a65cf595bc9343f63e1c6004d127fa2c89
HelgeBjork <helge@qualitypoint.se>
2019-08-27 10:11:55 +0200

    Have added some text and started on the userlist

--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -2,14 +2,27 @@ public class Main {
 
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Welcome");
+
+
+        User user = new User();
+
+
 
         Calendar calendar = new Calendar();
-
+        Thread.sleep(1000);
+        System.out.println("Available Time Slots");
         for(TimeSlot timeSlot : calendar.availableSlots()){
+
             System.out.println(timeSlot.toString());
         }
         Gui gui = new Gui(calendar);
         System.out.println(gui.toHtml());
+
+
     }
+
+
 }
--- src/java/main/UserList.java
+++ src/java/main/UserList.java
@@ -1,4 +1,15 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class UserList extends ArrayList<User> {
+
+    ArrayList<String> user = new ArrayList<String>();
+
+    user.add("");
+
+
+
+
 }
+
+
